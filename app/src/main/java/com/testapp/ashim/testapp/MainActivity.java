5a1c35d233bc95e22fca827b6d6e1f19856edc9f
ashimneupane <ashim.neupane@gmail.com>
2018-02-03 21:51:45 +0545

    initial commit

---
+++ app/src/main/java/com/testapp/ashim/testapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.testapp.ashim.testapp;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.Lifecycle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by awe on 3/2/18.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+}
